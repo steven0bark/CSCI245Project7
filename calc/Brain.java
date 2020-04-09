c8360ac8105f77a74e2efd83aa8707139677b0a9
Steven Barker <steven.barker@my.wheaton.edu>
2020-04-09 15:32:09 -0700

    Added constructor

---
+++ calc/Brain.java
@@ -0,0 +1,27 @@
+package calc;
+
+/**
+ * Brain.java
+ *
+ * This is the control of the Stats
+ *
+ *
+ *
+ */
+
+
+public class Brain {
+
+	private CalculatorFace face;	
+	
+	private State state;
+
+	public Brain(CalculatorFace f){
+		face = f;
+	}
+
+	public void switchState(State s){}
+
+
+
+}
