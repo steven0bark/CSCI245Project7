3f81aba29470ade1fb786db0bc1b127362e5a148
Steven Barker <steven.barker@my.wheaton.edu>
2020-04-22 20:02:33 -0700

    Final Documentation

--- calc/Brain.java
+++ calc/Brain.java
@@ -16,9 +16,6 @@ import java.text.DecimalFormat;
 public class Brain {
 
 
-//{{{Controller
-//{{{Instance Variables	
-
 	/**
 	 * The reference to the calculator face
 	 */
@@ -46,7 +43,7 @@ public class Brain {
 	
 	/**
 	 * The state for updating a non-whole number
-	 */
+	*/
 	private Decimal dec = new Decimal();
 
 	/**
@@ -85,15 +82,6 @@ public class Brain {
 	private DecimalFormat form = new DecimalFormat("#.####");
 	
 	/**
-	 * The current decimals place
-	 */
-	private int dplace = 0;
-//}}}
-	
-
-//{{{Brain Methods
-		
-	/**
 	 * Constructor
 	 *
 	 * @param f the CalculatorFace
@@ -115,7 +103,7 @@ public class Brain {
 	 *
 	 * @param num The number that is pressed
 	 */
-	public void operand(Double num){ operandstate.operand(num); }
+	public void operand(Double num){ output(operandstate.operand(num)); }
 
 	/**
 	 * When an operator is pressed, it will update the 
@@ -134,12 +122,12 @@ public class Brain {
 	 * When the plus minus button is pressed, the operand will be updated
 	 * and the posneg state will be updated.
 	 */
-	public void pm() { operandstate.plusminus(); posnegstate.pmUpdate(); }
+	public void pm() { operandstate.plusminus(); posnegstate = posnegstate.pmUpdate(); }
 	
 	/**
-	 * This method is for effiency in writing numbers to the screen 
+	 * This method is for efficency in writing numbers to the screen 
 	 *
-	 * @param num The number to be outputted
+	 * @param num The number to be outputed
 	 */
 	public void output(Double num) { face.writeToScreen(form.format(num)); }
 	
@@ -168,14 +156,11 @@ public class Brain {
 		operandstate = op1;
 		face.writeToScreen("");	
 	}
-//}}}
-//}}}
 
 	
-//{{{OpState
 	
 	/**
-	 * OpState.java
+	 * OpState.java.
 	 *
 	 * This is the superclass for the operand state
 	 * 
@@ -187,26 +172,47 @@ public class Brain {
 		 * The operand
 		 */
 		protected Double op = 0.0;
-	
+
 		/**
-		 * 
+		 * Updates the operand
+		 * @return the updated operand
 		 */
-		protected void operand(Double num) {
-			op = posnegstate.updateOperand(wholedecstate.modifyFirst(op), 
-					wholedecstate.modifySecond(num));
-			output(op);
+		protected Double operand(Double num) {
+			op = posnegstate.updateOperand(wholedecstate.modifyFirst(op), wholedecstate.modifySecond(num));
+			return op;
 		}
+
+		/**
+		 * Turns the operand either positive or negative
+		 */
 		protected void plusminus() { op *= -1; output(op); }
+
+		/**
+		 * @return The operand
+		 */
 		protected Double getOp() { return op; }
+
+		/**
+		 * Sets the operand
+		 */
 		protected void setOp(Double num) { op=num; }
+
+		/**
+		 * The evaluate method will usually do nothing unless it is 
+		 * the second operand
+		 */
 		protected void evaluate() {}
+
+		/**
+		 * Determines the functionality when an operator is pressed
+		 */
 		public abstract void updateOperator(SetUp.EvalStrat s);
 	}
 
 		
 		
 		/**
-		 * Op1.java
+		 * Op1.java.
 		 *
 		 * This is a subclass of OpState that is active when 
 		 * the calculator is working with the first operand
@@ -215,6 +221,9 @@ public class Brain {
 		 */
 	private class Op1 extends OpState{
 
+		/**
+		 * When an operand is pressed, it switches the OpState to Op2
+		 */
 		public void updateOperator(SetUp.EvalStrat s) {
 			eval = s;
 			posnegstate = pos;
@@ -225,7 +234,7 @@ public class Brain {
 	}
 		
 		/**
-		 * Op2.java
+		 * Op2.java.
 		 *
 		 * This the subclass of OpState that is active when
 		 * the calculator is working with the second operand
@@ -234,13 +243,18 @@ public class Brain {
 		 */
 	private class Op2 extends OpState {
 
-		@Override
+		/**
+		 * This evaluates the expression
+		 */
 		public void evaluate() {
 			op1.setOp(eval.evaluate());  
 			op= 0.0;
 			output(op1.getOp());
 		}
 
+		/**
+		 * When an operator is pressed, the expression will be evaluated
+		 */
 		public void updateOperator(SetUp.EvalStrat e) {
 			equal();
 			operandstate = this;
@@ -250,7 +264,7 @@ public class Brain {
 	}
 		
 		/**
-		 * InBetween.java
+		 * InBetween.java.
 		 *
 		 * This is the subclass of OpState that is active
 		 * between doing a calculation and doing something else
@@ -258,83 +272,178 @@ public class Brain {
 		 * @author stevenbarker
 		 */
 	public class InBetween extends OpState{
-			
-		@Override
-		public void operand(Double num) {
+	
+		/**
+		 * When an operand is pressed, it will clear the calculator and set the operand
+		 */
+		public Double operand(Double num) {
 			clear();
 			op1.setOp(num);
-			output(op1.getOp());
+			return op1.getOp();
 		}
 
+		/**
+		 * When an operator is pressed, it will set the state to Op2
+		 */
 		public void updateOperator(SetUp.EvalStrat s) { eval = s; operandstate = op2; }
+
 	}
 		
-//}}}		
 		
-		
-//{{{PosNegState		
 		
 		
 		/**
-		 * @author stevenbarker
+		 * PosNegState.java.
 		 *
-		 */
-		private abstract class PosNegState {
-			public abstract Double updateOperand(Double num1, Double num2);
-			public abstract void pmUpdate();
-		}
-
-			
-		/**
-		 * @author stevenbarker
-		 *
-		 */
-		private class Negative extends PosNegState {
-			public Double updateOperand(Double num1, Double num2) { return num1 - num2; }
-			public void pmUpdate() { posnegstate = pos; }			
-		}
-
-
-		/**
+		 * This class is the state to determine 
 		 * 
 		 * @author stevenbarker
+		 */
+		private abstract class PosNegState {
+			
+			/**
+			 * Will either add or subtract the two numbers
+			 *
+			 * @return The number 
+			 */
+			public abstract Double updateOperand(Double num1, Double num2);
+			
+			/**
+			 * Switches the states
+			 */
+			public abstract PosNegState pmUpdate();
+		}
+
+			
+		/**
+		 * Negative.java.
 		 *
+		 * This class is for updating a negative operand
+		 *
+		 * @author stevenbarker
+		 */
+		private class Negative extends PosNegState {
+			
+			/**
+			 * Subtracts the two numbers
+			 *
+			 * @return The updated operand
+			 */
+			public Double updateOperand(Double num1, Double num2) { return num1 - num2; }
+
+			/**
+			 * Switches the state
+			 */
+			public PosNegState pmUpdate() { return pos; }
+		}
+
+
+		/**
+		 * Positive.java.
+		 *
+		 * This class is for updating a positive operand
+		 *
+		 * @author stevenbarker
 		 */
 		private class Positive extends PosNegState{
+			/**
+			 * Adds the two numbers
+			 *
+			 * @return the operand
+			 */
 			public Double updateOperand(Double num1, Double num2) { return num1 + num2; }
-			public void pmUpdate() { posnegstate = neg; }
+
+			/**
+			 * Switches the states
+			 */
+			public PosNegState pmUpdate() { return  neg; }
 		}
-///}}}			
 			
-			
-//{{{WholeDecState			
 			
 
+		/**
+		 * WholeDecState.java.
+		 *
+		 * This class is the state for modifying two numbers to be
+		 * given to the posnegstate to either be added or subtracte
+		 *
+		 * @author stevenbarker
+		 *
+		 */
 		private abstract class WholeDecState {
+
+			/**
+			 * The current decimals place 
+			 */
 			protected int dplace = 0;
+
+			/**
+			 * Resets the decimals place
+			 */
 			protected void resetDPlace() { dplace = 0; }
+
+			/**
+			 * Changes the first number in updating the operand into a form where it
+			 * can be given to another method to simply added or subtracted from the 
+			 * second number
+			 *
+			 * @return the modified number
+			 */
 			public abstract Double modifyFirst(Double num);
+			
+			/**
+			 * Changes the second  number in updating the operand into a form where it
+			 * can be given to another method to simply added or subtracted from the 
+			 * first number
+			 *
+			 * @return The modified number
+			 */
 			public abstract Double modifySecond(Double num);
+
 		}	
 			
 		/**
+		 * Whole.java.
+		 * 
+		 * This class modfiies the number for a whole operand
+		 *
 		 * @author stevenbarker
 		 *
 		 */
 		private class Whole extends WholeDecState {
+
+			/**
+			 * Multiplies the operand by 10
+			 */
 			public Double modifyFirst(Double num) { return num*10; }
+
+			/**
+			 * Just returns the same number
+			 */
 			public Double modifySecond(Double num) { return num; }
 		}
 				
 		/**
-		* @author stevenbarker
+		* Decimal.java.
 		*
+		* This class is for modifying numbers if the operand is not whole
+		*
+		* @author stevenbarker
 		*/
 		private class Decimal extends WholeDecState {
+
+			/**
+			 * Just returns the number
+			 */
 			public Double modifyFirst(Double num) { return num; }
+
+			/**
+			 * Multilplies the number by 10^-decimal places so it can be properly added
+			 * to the operand
+			 */
 			public Double modifySecond(Double num) { dplace++; return num * Math.pow(10, -dplace); }
+			
 		}
 				
-//}}}
 		
 }
